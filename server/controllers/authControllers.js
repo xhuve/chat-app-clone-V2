23e4c3a2888c8983649d92a312466f897d359ee7
xhuve <xhuvelialbana@gmail.com>
2024-07-07 20:15:36 +0200

    Create user model and implement simple authentications

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,17 +1,79 @@
+import bcrypt from 'bcryptjs'
+import User from '../models/userModel.js'
+import generateToken from '../utils/generateToken.js'
 
-
-export const signup = (req, res) => {
+export const signup = async (req, res) => {
     try {
-        const {username, password} = req.body
+        const {fullname, username, password, confirmPassword, gender} = req.body
+        if (password !== confirmPassword)
+            return res.status(400).json({error: "Password do not match"})
+ 
+        const user = await User.findOne({username})
+
+        if (user)
+            return res.status(400).json({error: "Username already exists"})
+
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
+        const girlProfilepic = `https://avatar.iran.liara.run/public/girl?username=${username}`
+
+        const newUser = new User({
+            fullname,
+            username,
+            password: hashedPassword,
+            gender,
+            profilePic: gender === "male" ? boyProfilePic : girlProfilepic
+        })
+
+        if (newUser){
+            generateToken(newUser._id, res)
+            await newUser.save()
+            
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullname,
+                username: newUser.username,
+                profilePic: newUser.profilePic
+            })
+        } else{
+            res.status(400).json({error: "Invalid user data"})
+        }
+
     } catch (error) {
-        
+        res.status(400).json({error: "Internal Server Error" + error})
     }
 }
 
-export const login = (req, res) => {
-    res.send("login")
+export const login = async (req, res) => {
+    try {
+        const {username, password} = req.body
+        const user = await User.findOne({username})
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "")
+
+        if (!user || !isPasswordCorrect)
+            return res.status(400).json({error: "Invalid credentials"})
+
+        generateToken(user._id, res)
+
+        res.status(200).send({
+            _id: user._id,
+            fullName: user.fullname,
+            username: user.username,
+            profilePic: user.profilePic
+        })
+
+    } catch (error) {
+        res.status(400).json({error: "Internal Server Error" + error})
+    }
 }
 
 export const logout = (req, res) => {
-    res.send("logout")
+    try {
+        res.cookie("jwt", "", { maxAge: 0 })
+        res.status(200).json({ message: "Logged out successfully" })
+    } catch (error) {
+        res.status(400).json({error: "Internal Server Error" + error})    
+    }
 }
--- server/server.js
+++ server/server.js
@@ -1,16 +1,20 @@
 import express from "express"
 import dotenv from 'dotenv'
+
 import authRoutes from "./routes/authRoutes.js"
 import connectToMongoDb from "./database/connectToMongoDB.js"
 
 dotenv.config()
 const app = express()
 
+app.use(express.json())
+
+app.use("/api/auth", authRoutes)
+
 app.get('/', (req, res) => {
     res.send("Hello!")
 })
 
-app.use("/api/auth", authRoutes)
 
 
 const PORT = process.env.PORT || 3000
